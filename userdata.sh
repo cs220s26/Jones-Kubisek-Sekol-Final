c82d708f4fdb1650a7d4f4b7188d4aa32c05eeee
Joshua Jones <jonesj11@moravian.edu>
2026-04-29 21:38:14 -0400

    Service file test 1

--- userdata.sh
+++ userdata.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 dnf update -y
-dnf install -y redis6
-yum install -y git
+dnf install -y redis6 git maven-amazon-corretto21
+
+cd /home/ec2-user
 git clone https://github.com/cs220s26/Jones-Kubisek-Sekol-Final.git
-yum install -y maven-amazon-corretto21
-cd /Jones-Kubisek-Sekol-Final
+cd /home/ec2-user/Jones-Kubisek-Sekol-Final
 mvn package
+
 systemctl start redis6
 systemctl enable redis6 
-TODO: .service file information
+
+sudo cp jeopardy_bot.service /etc/systemd/system/tunetailorbot.service
+sudo systemctl daemon-reload
+sudo systemctl enable jeopardy_bot
