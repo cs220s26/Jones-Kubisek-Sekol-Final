9194d441a1a8f91defc745d0aabc658e5da64936
Lilian Kubisek <kubisekl@moravian.edu>
2026-05-04 10:53:48 -0400

    added other bash files to scripts folder

---
+++ scripts/userdata.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+dnf update -y
+dnf install -y redis6 git maven-amazon-corretto21
+
+cd /home/ec2-user
+git clone https://github.com/cs220s26/Jones-Kubisek-Sekol-Final.git
+cd /home/ec2-user/Jones-Kubisek-Sekol-Final
+mvn package
+chmod +x redeploy.sh
+
+systemctl start redis6
+systemctl enable redis6 
+
+cp /home/ec2-user/Jones-Kubisek-Sekol-Final/jeopardy_bot.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable jeopardy_bot
+systemctl start jeopardy_bot
+
